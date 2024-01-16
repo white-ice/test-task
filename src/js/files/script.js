0865b2c69c869a2c4311025fa9c2d306bf46c333
Valentin <vtonkopey@gmail.com>
2024-01-16 17:36:23 +0200

    added Assembly structure

---
+++ src/js/files/script.js
@@ -0,0 +1,25 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+// Clock
+// function clock() {
+// 	const hours = document.getElementById('hours')
+// 	const minutes = document.getElementById('minutes')
+// 	const seconds = document.getElementById('seconds')
+
+// 	let h = new Date().getHours();
+// 	let m = new Date().getMinutes();
+// 	let s = new Date().getSeconds();
+
+// 	h = h < 10 ? "0" + h : h;
+// 	m = m < 10 ? "0" + m : m;
+// 	s = s < 10 ? "0" + s : s;
+
+// 	hours.innerHTML = h;
+// 	minutes.innerHTML = m;
+// 	seconds.innerHTML = s;
+// }
+
+// const interval = setInterval(clock, 1000);
